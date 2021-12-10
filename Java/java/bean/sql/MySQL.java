31bff162435ac01b2e7cd94a0f78518d2f5a68b1
nekopaparo <89907578+nekopaparo@users.noreply.github.com>
2021-12-10 17:01:33 +0800

    MySQL

--- Java/java/bean/sql/MySQL.java
+++ Java/java/bean/sql/MySQL.java
@@ -1,8 +1,11 @@
-package bean.sql;
+package base.bean.sql;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MySQL {
 	private Connection conn = null;
@@ -16,18 +19,10 @@ public class MySQL {
 	// 使用者資料
 	private String user = "root";
 	private String password = "1234";
-
-	public void setdb(String db) {
-		this.db = db;
-	}
-
-	public void getdb() {
-		System.out.println("資料庫名稱 : " + db);
-	}
-
+	
 	// 如使用EclipseWeb，需將lib/JDBC的jar放入所使用的Apache/lib中才能使用driver
 	// 不過Tomcat有內建，所以沒放也可以使用DriverManager.getConnection
-	public Connection getCon() {
+	MySQL() {
 		try {
 			Class.forName(driver);
 			conn = DriverManager.getConnection(url, user, password);
@@ -37,6 +32,60 @@ public class MySQL {
 		} catch (SQLException e) {
 			System.out.println("conn有問題");
 		}
-		return conn;
+	}
+	public void setdb(String db) {
+		this.db = db;
+	}
+
+	public void getdb() {
+		System.out.println("資料庫名稱 : " + db);
+	}
+
+	public Connection getCon() { return conn; }
+
+	// 輸出
+	public void executeQuery(String sql) {
+		try {
+			Statement sm = conn.createStatement();
+			ResultSet rs = sm.executeQuery(sql);
+
+			// 標題
+			ResultSetMetaData rsmd = rs.getMetaData();
+			int column = rsmd.getColumnCount();
+			for (int i = 1; i < column; i++) {
+				System.out.print(rsmd.getColumnName(i) + "\t");
+			}
+			System.out.println();
+			// 內容
+			while (rs.next()) {
+				for (int i = 1; i < column; i++) {
+					System.out.print(rs.getString(i) + "\t");
+				}
+				System.out.println();
+			}
+		} catch (SQLException e) {
+			System.out.println("輸出失敗");
+			e.printStackTrace();
+		}
+
+	}
+
+	// 輸入
+	public void executeUpdate(String sql) {
+		try {
+			Statement sm = conn.createStatement();
+			switch (sm.executeUpdate(sql)) {
+			case 0:
+				System.out.println("輸入失敗");
+				break;
+			case 1:
+				System.out.println("輸入成功");
+				break;
+			}
+		} catch (SQLException e) {
+			System.out.println("輸入失敗");
+			e.printStackTrace();
+		}
+
 	}
 }
--- Java/java/bean/sql/README.md
+++ Java/java/bean/sql/README.md
@@ -4,3 +4,16 @@
 |[MariaDB](./MariaDB.java)|mariadb-java-client|[download](https://mariadb.com/downloads/connectors/connectors-data-access/java8-connector/)|
 |[MySQL](./MySQL.java)|mysql-connector-java|.\MySQL\Connector J 8.0|
 |[SQLite](./SQLite.java)|sqlite-jdbc |[download](https://github.com/xerial/sqlite-jdbc/releases)|
+
+# code use
+## MySQL
+```java
+MySQL mysql = new MySQL();
+		
+String add = "INSERT neko(id, name, tel) VALUES(1, 'cat', '0800')";
+mysql.executeUpdate(add);
+String show = "SELECT * FROM neko";
+mysql.executeQuery(show);
+String delete = "DELETE FROM neko WHERE id >= 0";
+mysql.executeUpdate(delete);
+```
