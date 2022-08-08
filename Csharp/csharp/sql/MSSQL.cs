65a4ab1319e10f615959a573d0b8e459b0f1b893
nekopaparo <89907578+nekopaparo@users.noreply.github.com>
2022-08-08 20:51:48 +0800

    C#SQL

--- Csharp/csharp/sql/MSSQL.cs
+++ Csharp/csharp/sql/MSSQL.cs
@@ -1,27 +1,27 @@
-﻿/// https://www.delftstack.com/zh-tw/howto/csharp/connect-to-sql-database-from-csharp/
-/// https://stackoverflow.com/questions/25739788/select-query-to-get-data-from-sql-server
-using System;
+﻿using System;
 using System.Data.SqlClient;
 
-namespace SQL
+namespace FarmProductHTML.Services
 {
-    class MSSQL
+    public class MSSQL
     {
+        // The server's name
+        private string DataSource = "NEKOMIMIRO\\SQLEXPRESS";
+
+        // The name of the database
+        private string InitialCatalog = "FarmProductHTML";
+
+        // Windows 驗證
+        private string IntegratedSecurity = "SSPI";
+
+        // SQL Server 驗證
+        private string UserID = "root";
+        private string Password = "1234";
+
+        private SqlConnection connection = null;
+
         public MSSQL()
         {
-            // The server's name
-            string DataSource = "NEKOMIMIRO\\SQLEXPRESS";
-
-            // The name of the database
-            string InitialCatalog = "FarmProductHTML";
-
-            // Windows 驗證
-            string IntegratedSecurity = "SSPI";
-
-            // SQL Server 驗證
-            string UserID = "root";
-            string Password = "1234";
-            
             // url
             string connectionString = "Data Source = " + DataSource
                                     + "; Initial Catalog = " + InitialCatalog
@@ -31,61 +31,72 @@ namespace SQL
                                     + "; Password = " + Password
                                     /// END
                                     ;
-
-            // 使用資料庫
-            SqlConnection connection = null;
             try
             {
                 // 載入 Connection
                 connection = new SqlConnection(connectionString);
                 connection.Open();
-                Console.WriteLine("The database has been opened!");
-                Console.WriteLine("Connection State: " + connection.State.ToString());
-
-                // 查詢 Query
-                SqlCommand command = new SqlCommand("SELECT * FROM FPHProductDataVIEW", connection);
-                int result = command.ExecuteNonQuery();
-                using (SqlDataReader reader = command.ExecuteReader())
-                {
-                    // Name
-                    for (int i = 0; i < reader.FieldCount; i += 2)
-                    {
-                        Console.Write(String.Format("{0}\t", reader.GetName(i)));
-                    }
-                    Console.WriteLine();
-                    // Value
-                    while (reader.Read())
-                    {
-                        for (int i = 0; i < reader.FieldCount; i += 2)
-                        {
-                            Console.Write(String.Format("{0}\t", reader.GetValue(i)));
-                        }
-                        Console.WriteLine();
-                    }
-                }
             }
             catch (Exception ex)
             {
-                Console.WriteLine("There's an error connecting to the database!\n" + ex.Message);
+                Console.WriteLine("Open Error! -> " + ex.Message);
             }
-            finally
+        }
+        ~MSSQL()
+        {
+            Dispose();
+        }
+        public void Dispose()
+        {
+            if (connection != null)
             {
-                if (connection != null)
+                if (connection.State == System.Data.ConnectionState.Open)
                 {
-                    if (connection.State == System.Data.ConnectionState.Open)
-                    {
-                        connection.Close();
-                        Console.WriteLine("The database has been closed!");
-
-                        connection.Dispose();
-                        Console.WriteLine("The database connection has been disposed!");
-                        Console.WriteLine("Connection State: " + connection.State.ToString());
-                    }
-                    connection = null;
+                    connection.Close();
+                    connection.Dispose();
                 }
+                connection = null;
             }
-            Console.WriteLine("END");
-            Console.ReadLine();
+        }
+
+        public SqlCommand GetCommand(string sql)
+        {
+            try
+            {
+                return new SqlCommand(sql, connection);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Command Error! -> " + ex.Message);
+                return null;
+            }
+        }
+        // 加入 Parameter
+        public static void AddParm(SqlCommand cmd, string parmName, object parmValues)
+        {
+            cmd.Parameters.Add(new SqlParameter(parmName, parmValues));
+        }
+        public static void AddParm(SqlCommand cmd, string parmName, System.Data.SqlDbType type, object parmValue)
+        {
+            cmd.Parameters.Add(parmName, type).Value = parmValue;
+        }
+        // 取得資料
+        public SqlDataReader GetDataReader(SqlCommand cmd)
+        {
+            try
+            {
+                return cmd?.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("DataReader Error! -> " + ex.Message);
+                return null;
+            }
+        }
+        // 查詢
+        public SqlDataReader Query(string sql)
+        {
+            return GetCommand(sql)?.ExecuteReader();
         }
     }
 }
